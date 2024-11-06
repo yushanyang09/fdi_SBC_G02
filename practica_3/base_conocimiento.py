67dce19ddd99a711702a1bd7c28bcaf4dc8025bf
yushyng <yushanyang05@gmail.com>
2024-11-06 12:58:33 +0100

    modificada la estructura del motor de conocimiento

--- practica_3/base_conocimiento.py
+++ practica_3/base_conocimiento.py
@@ -6,7 +6,7 @@ def leer_base_conocimiento(base):
     base (Path): ruta al fichero de la base de conocimiento"""
 
     # Diccionario que contendrá la base de conocimiento
-    base_dict = {}
+    base_list=[]
 
     # Leemos el fichero que contiene la base de conocimiento
     with base.open("r", encoding="utf-8") as f:
@@ -21,36 +21,28 @@ def leer_base_conocimiento(base):
         
         # Separamos los elementos de la línea
         line_items = line.split()
-        
         if len(line_items) == 4:
             sujeto, predicado, objeto, fin = line_items
             # Si el sujeto no está en la base lo añadimos
-            if sujeto not in base_dict:
-                base_dict[sujeto] = {}
-            anyadir_afirmacion(base_dict, sujeto, predicado, objeto)
-            
-        elif len(line_items) == 3:
+            sujeto_auxiliar=sujeto
+            afirmacion=(sujeto, predicado, objeto)
+            anyadir_afirmacion(base_list, afirmacion)
+        if len(line_items) == 3:
             predicado, objeto, fin = line_items
-            anyadir_afirmacion(base_dict, sujeto, predicado, objeto)
+            afirmacion=(sujeto_auxiliar, predicado, objeto)
+            anyadir_afirmacion(base_list, afirmacion)
     
-    return base_dict
+    return base_list
 
-def anyadir_afirmacion(base_dict, sujeto, predicado, objeto):
+def anyadir_afirmacion(base_list, afirmacion):
     """Añade una afirmación a la base de conocimiento.
     
     Parámetros:
-    base_dict (dict): diccionario de la base de conocimiento
+    base_dict (dict): lista de la base de conocimiento
     sujeto (string): entidad que hace de sujeto de la afirmación
     predicado (string): relación de la afirmación
     objeto (string): entidad o atributo
     """
-            
-    # Si el predicado existe, añadimos el objeto
-    if predicado in base_dict[sujeto]:
-        # El objeto se añade solo si no estaba ya en el set (para evitar repetidos)
-        base_dict[sujeto][predicado].add(objeto)
-
-    # Si no, añadimos la relación y el objeto
-    else:
-        base_dict[sujeto][predicado] = set() # conjunto
-        base_dict[sujeto][predicado].add(objeto)
+    if afirmacion not in base_list:
+        base_list.append(afirmacion)
+    return base_list
