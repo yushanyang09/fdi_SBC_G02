4c8d55882bce5da4f39f8462037da5b7de533bba
carfer23 <carfer23@ucm.es>
2024-11-13 15:55:23 +0100

    Añadida la implementación que hicimos en clase

--- practica_3/motor_consultas.py
+++ practica_3/motor_consultas.py
@@ -24,6 +24,11 @@ def leer_consulta(texto_consulta):
 
     return variables_select, tuplas_where
 
+def es_variable(elemento):
+    """Determina si un elemento de una tupla es una variable."""
+
+    return elemento.startswith('?')
+
 def resolver_consulta(afirmaciones, consulta):
     """Resuelve una consulta buscando iterativamente una afirmación de la base de
     conocimiento que coincida con lo que se pregunta en la consulta.
@@ -37,29 +42,33 @@ def resolver_consulta(afirmaciones, consulta):
     """
 
     respuestas = []
-    tupla_booleana = tuple(elemento.startswith('?') for elemento in consulta)
 
     for afirmacion in afirmaciones:
 
-        condicion_sujeto = (afirmacion[0] == consulta[0]) | tupla_booleana[0]
-        condicion_relacion = (afirmacion[1] == consulta[1]) | tupla_booleana[1]
-        condicion_objeto = (afirmacion[2] == consulta[2]) | tupla_booleana[2]
+        condicion_sujeto = (afirmacion[0] == consulta[0]) or es_variable(consulta[0])
+        condicion_relacion = (afirmacion[1] == consulta[1]) or es_variable(consulta[1])
+        condicion_objeto = (afirmacion[2] == consulta[2]) or es_variable(consulta[2])
 
-        if (condicion_sujeto & condicion_relacion & condicion_objeto):
+        if (condicion_sujeto and condicion_relacion and condicion_objeto):
             respuestas.append(afirmacion)
     
     return respuestas
 
-def imprimir_respuestas(respuestas, variables_select, indice_a_variable):
-    """Provisional: Imprime las respuestas basadas en las variables seleccionadas"""
-    for respuesta in respuestas:
-        salida = ", ".join(
-            f"{variable}: {respuesta[indice_a_variable[variable]]}" 
-            for variable in variables_select
-            if variable in indice_a_variable
-        )
-        print(salida)
+def imprimir_respuestas(variables_usadas, indices_validos, variables_select):
+    """Imprime los valores de las variables de las respuestas.
+    
+    Parámetros:
+    - variables_usadas (dict):
+    - indices_validos (set):
+    - variables_select (lista):
+    """
 
+    for indice in indices_validos:
+        for variable in variables_select:
+            #if (indice in variables_usadas[variable].values()): # creo que no hace falta
+                # Nos quedamos con los valores de las variables (las claves del diccionario) que tengan este indice como valor
+                respuestas = [clave for clave, val in variables_usadas[variable].items() if val == indice]
+                print(respuestas)
 
 def procesar_consulta(bc, texto_consulta):
     """Procesa una consulta a la base de conocimiento introducida por el usuario.
@@ -72,66 +81,60 @@ def procesar_consulta(bc, texto_consulta):
     # Parseamos el texto de la consulta
     variables_select, tuplas_where = leer_consulta(texto_consulta)
 
-    # Diccionario para almacenar los valores válidos para cada variable
+    # Diccionario para almacenar los valores posibles de cada variable
+    # Las claves son las variables (por ejemplo, "?profesor")
+    # El valor es otro diccionario cuyas claves son los posibles valores de esa variable 
+    # junto con su índice (por ejemplo, {"alberto":0, "antonio":1})
     variables_usadas = {}
 
-    # Mapeo de las variables a sus posiciones en las tuplas (para imprimir_respuestas)
-    indice_a_variable ={var: None for var in variables_select}
+    # Asignamos un índice único a cada valor de una variable
+    indice = 0
 
-     # Lista para almacenar las respuestas que resulten válidas
-    respuestas_validas = []
+    # Resolvemos la primera sentencia
+    tupla_inicial = tuplas_where[0]
+    respuestas_iniciales = resolver_consulta(bc, tupla_inicial)
 
-    # Una tupla es una sentencia dentro del where que tiene 3 elementos (sujeto, predicado, objeto)
-    for tupla in tuplas_where:
+    for respuesta_inicial in respuestas_iniciales:
+            for i, elemento in enumerate(tupla_inicial):
+                if es_variable(elemento):
+                    if elemento not in variables_usadas:
+                        variables_usadas[elemento] = {}
+                    variables_usadas[elemento][respuesta_inicial[i]] = indice
+            indice += 1
 
-        # Qué elementos de la tupla son variables
-        variables_tupla = tuple(elemento.startswith('?') for elemento in tupla)
+    # Resolvemos las siguientes sentencias si las hay
+    for tupla in tuplas_where[1:]:
 
-       # Asocia cada variable a su posición en la tupla
-        for j, elemento in enumerate(tupla):
-            if variables_tupla[j] and elemento in variables_select:
-                indice_a_variable[elemento] = j
+        nuevas_respuestas = resolver_consulta(bc, tupla)
 
-        # Resolvemos la sentencia
-        respuestas = resolver_consulta(bc, tupla)
+        respuestas_validas = []
 
+        # Seleccionamos solo las respuestas válidas según las respuestas iniciales
+        for nueva_respuesta in nuevas_respuestas:
+            for i, elemento in enumerate(tupla):
+                if es_variable(elemento):
+                    if elemento in variables_usadas:
+                        if nueva_respuesta[i] in variables_usadas[elemento]:
+                            respuestas_validas.append(nueva_respuesta)
+        
+        # Conjunto para almacenar los índices de las respuestas válidas
+        indices_validos = set()
 
-        # Se pone a True la primera vez que una variable aparezca en una sentencia del WHERE
-        # WHERE { ?personaje t2:pertenece q2:casa_stark . } -> [True, False, False]
-        variables_nuevas = [False, False, False]
+        # Si una variable no se ha usado antes, se pone su posición a True
+        variable_nueva = [False, False, False]
 
-        for respuesta in respuestas:
-
-            valida = True
-
-            # Si el valor de una variable no es uno de los posibles valores almacenados para 
-            # esa variable, la respuesta no es válida
-            # Para cada elemento de una sentencia (sujeto, predicado, objeto)
-            for j, elemento in enumerate(tupla):
-                # Si es una variable
-                if variables_tupla[j]:
-                    # Si la variable ya fue usada
-                    if elemento in variables_usadas and not variables_nuevas[j]:
-                        # Si el valor en la respuesta no es uno de los posibles valores
-                        # obtenidos de una consulta sobre una sentencia anterior
-                        if respuesta[j] not in variables_usadas[elemento]:
-                            # La respuesta no es válida
-                            valida = False
-                    # Si la variable no existe en variables_usada (no ha sido usada por una 
-                    # sentencia anterior)
-                    else:
-                        if not variables_nuevas[j]:
-                            # Se inicializa la lista de valores posibles de la variable
-                            variables_usadas[elemento] = []
-                            variables_usadas[elemento].append(respuesta[j])
-                            variables_nuevas[j] = True
+        for respuesta_valida in respuestas_validas:
+            for i, elemento in enumerate(tupla):
+                if es_variable(elemento):
+                    if elemento in variables_usadas:
+                        if variable_nueva[i] == False:
+                            indice2 = variables_usadas[elemento][respuesta_valida[i]]
+                            indices_validos.add(indice2)
                         else:
-                            # Se añade el nuevo valor posible de la variable
-                            variables_usadas[elemento].append(respuesta[j])
-
-            # Si la respuesta es válida la añadimos a la lista    
-            if valida:
-                respuestas_validas.append(respuesta)
-    print(variables_usadas)
-
-    imprimir_respuestas(respuestas_validas, variables_select, indice_a_variable)
+                            variables_usadas[elemento][respuesta_valida[i]] = indice2
+                    else:
+                        variables_usadas[elemento] = {}
+                        variables_usadas[elemento][respuesta_valida[i]] = indice2
+                        variable_nueva[i] = True
+        
+        imprimir_respuestas(variables_usadas, indices_validos, variables_select)
