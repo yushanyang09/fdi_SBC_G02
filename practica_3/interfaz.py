1cbf2bbc989c994be787b99ab815d5040ec1c533
yushyng <yushanyang05@gmail.com>
2024-11-17 22:11:11 +0100

    añadido comando multilinea y modificado la base de peliculas

--- practica_3/interfaz.py
+++ practica_3/interfaz.py
@@ -9,9 +9,21 @@ def bienvenida():
     print("BIENVENIDO! \U0001F600")
 
 def introducir_comando():
-    print("Introduce un comando:")
-    return input()
-
+    """ Permite introducir comandos en una sola línea o múltiples líneas si terminan con `}`."""
+    print("Introduce tu comando (finaliza con '}' si es multilinea):")
+    
+    # Inicializamos las variables para acumular líneas
+    comando = []
+    while True:
+        linea = input()
+        comando.append(linea.strip())
+        
+        # Si la última línea contiene '}', asumimos que el comando está completo
+        if linea.strip().endswith('}'):
+            break
+    
+    # Unimos las líneas en un único comando
+    return " ".join(comando)
 def imprimir_ayuda():
     """Imprime la ayuda al usuario definida como docstring en el main"""
     ctx = click.get_current_context()
